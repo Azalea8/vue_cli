4ff068ae0668176a2b588baf66f252bd6be527b0
Azalea <2574292235@qq.com>
2023-06-07 18:57:31 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+/*
+    该文件是整个项目的入口文件
+*/
+// 引入Vue
 import Vue from 'vue'
+// 引入App,所有组件的父组件
 import App from './App.vue'
-
+// 关闭Vue生产提示
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App),
+})
