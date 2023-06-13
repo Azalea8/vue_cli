2d08e4d5d737a3c7431a4f5623570d2341343426
Azalea <2574292235@qq.com>
2023-06-13 11:10:21 +0800

    Vue脚手架

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <Header></Header>
-    <List></List>
-    <Footer></Footer>
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <Header :addTodoObj="addTodoObj"/>
+        <List :todos="todos" :checkTodoObj="checkTodoObj"/>
+        <Footer/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,9 +22,66 @@ export default {
     List,
     Footer,
   },
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  methods: {
+    addTodoObj(todoObj){
+      this.todos.unshift(todoObj)
+    },
+    checkTodoObj(id) {
+      this.todos.forEach((todo) => {
+        if(todo.id === id) todo.done = !todo.done
+      })
+    }
+  }
 }
 </script>
 
 <style>
+/*base*/
+body {
+  background: #fff;
+}
 
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,7 @@
 <template>
-  <Item></Item>
+  <ul class="todo-main">
+    <Item v-for="todo in todos" :key="todo.id" :todo="todo" :checkTodoObj="checkTodoObj"/>
+  </ul>
 </template>
 
 <script>
@@ -8,10 +10,26 @@ export default {
   name: 'List',
   components: {
     Item,
-  }
+  },
+  props:['todos', 'checkTodoObj']
 }
 </script>
 
-<style>
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0;
+}
 
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
 </style>
