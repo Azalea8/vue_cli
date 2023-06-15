f339f93f4108f897d933ed0d1e98395371f65176
Azalea <2574292235@qq.com>
2023-06-15 23:33:28 +0800

    Vue脚手架

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,34 @@
 <template>
-  <div>
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <Header @addTodoObj="addTodoObj"/>
-        <List :todos="todos" :checkTodoObj="checkTodoObj" :deleteTodoObj="deleteTodoObj"/>
-        <Footer :todos="todos" @checkAllTodoObj="checkAllTodoObj" @clearAllTodoObj="clearAllTodoObj"/>
-      </div>
-    </div>
+  <div class="app">
+    <h1>{{msg}}<span v-show="studentName">{{studentName}}</span></h1>
+    <Student></Student>
+    <br/>
+    <School></School>
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-import List from "@/components/List.vue";
-import Footer from "@/components/Footer.vue";
+import Student from "@/components/Student.vue";
+import School from "./components/School.vue";
 
 export default {
   name: 'App',
   components: {
-    Header,
-    List,
-    Footer,
+    School,
+    Student,
   },
   data() {
-    return {
-      todos: [],
+    return{
+      msg: '你好啊！',
+      studentName: '',
     }
   },
-  methods: {
-    addTodoObj(todoObj) {
-      this.todos.unshift(todoObj)
-    },
-    checkTodoObj(id) {
-      this.todos.forEach((todo) => {
-        if (todo.id === id) todo.done = !todo.done
-      })
-    },
-    deleteTodoObj(id) {
-      this.todos = this.todos.filter((todo) => {
-        return todo.id !== id
-      })
-    },
-    checkAllTodoObj(done) {
-      this.todos.forEach((todo) => {
-        todo.done = done
-      })
-    },
-    clearAllTodoObj() {
-      this.todos = this.todos.filter((todo) => {
-        return !todo.done
-      })
-    }
-  },
-  watch: {
-    todos: {
-      deep: true,
-      handler(newValue) {
-        localStorage.setItem('todos', JSON.stringify(newValue))
-      }
-    }
-  },
-  created() {
-    this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-  }
 }
 </script>
 
 <style>
-/*base*/
-body {
-  background: #fff;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
+  .app{
+    background: gray;
+    padding: 5px;
+  }
 </style>
---
+++ src/components/School.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="demo">
+  <h1>{{msg}}</h1>
+  <h2>学校名称: {{name}}</h2>
+  <h2>学校地址: {{address}}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "School",
+  data() {
+    return{
+      msg: '我是一个南山中学的学生',
+      name: '南山中学',
+      address: '四川省绵阳市',
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .demo {
+    background: orange;
+    padding: 5px;
+    margin: 5px;
+  }
+</style>
