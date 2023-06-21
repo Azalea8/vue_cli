e415903cccaf16bacdd2c237bb988af9e70220a4
Azalea <2574292235@qq.com>
2023-06-21 21:09:56 +0800

    Vue脚手架

---
+++ 22_插槽/01_默认插槽/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <Category title="美食" :dateList="foods">
+      <ul>
+        <li v-for="(date,index) in foods" :key="index">
+          {{date}}
+        </li>
+      </ul>
+    </Category>
+    <Category title="游戏" :dateList="games">
+      <ul>
+        <li v-for="(date,index) in games" :key="index">
+          {{date}}
+        </li>
+      </ul>
+    </Category>
+    <Category title="电影" :dateList="films">
+      <ul>
+        <li v-for="(date,index) in films" :key="index">
+          {{date}}
+        </li>
+      </ul>
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from "@/components/Category.vue";
+
+export default {
+  name: 'App',
+  components: {Category},
+  template: {
+    Category,
+  },
+  data() {
+    return {
+      foods: ['火锅', '烧烤', '小龙虾', '牛排'],
+      games: ['红色警戒', '穿越火线', '劲舞团', '超级玛丽'],
+      films: ['《教父》', '《拆弹专家》', '《你好，李焕英》', '《尚硅谷》']
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ 22_插槽/01_默认插槽/components/Category.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="category">
+    <h3>{{title}}</h3>
+    <slot>我算是插槽</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category-',
+  props: ['dateList','title']
+}
+</script>
+
+<style>
+.category{
+  background: skyblue;
+  width: 200px;
+  height: 300px;
+}
+
+h3{
+  text-align: center;
+  background: orange;
+}
+</style>
