cab4f64ef42470ac068ba91e27612f6792dd6864
Azalea <2574292235@qq.com>
2023-06-22 17:01:07 +0800

    Vue脚手架

---
+++ 23_src_Vuex/components/Count.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>当前求和为：{{$store.state.sum}}</h1>
+    <select v-model.number="num">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Count-',
+  data() {
+    return {
+      num: 1,
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.commit('INCREMENT', this.num)
+    },
+    decrement() {
+      this.$store.commit('DECREMENT', this.num)
+    },
+    incrementOdd() {
+      this.$store.dispatch('incrementOdd', this.num)
+    },
+    incrementWait() {
+      this.$store.dispatch('incrementWait', this.num)
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
