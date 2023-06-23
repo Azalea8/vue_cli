a3343e53ad36df78111633066e51e9bbe4db115b
Azalea <2574292235@qq.com>
2023-06-23 17:58:21 +0800

    Vue脚手架

---
+++ 25_src_求和案例_mapMutations_mapActions/store/index.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex)
+
+const actions = {
+    /*increment(context, value) {
+        context.commit('INCREMENT', value)
+    },
+    decrement(context, value) {
+        context.commit('DECREMENT', value)
+    },*/
+    incrementOdd(context, value) {
+        if (context.state.sum % 2) {
+            context.commit('INCREMENT', value)
+        }
+    },
+    incrementWait(context, value) {
+        setTimeout(() => {
+            context.commit('INCREMENT', value)
+        }, 500)
+    }
+}
+
+const mutations = {
+    INCREMENT(state, value) {
+        state.sum += value
+    },
+    DECREMENT(state, value) {
+        state.sum -= value
+    },
+
+}
+
+const state = {
+    sum: 0,
+}
+
+const getters = {
+    bigSum(state) {
+        return state.sum * 10
+    }
+}
+
+export default new Vuex.Store({
+    actions: actions,
+    mutations: mutations,
+    state: state,
+    getters: getters,
+})
